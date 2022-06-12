1abae7421803fc9d887deeb357da9c66ddc02fb2
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2022-06-13 01:14:25 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route, Link
 } from "react-router-dom";
 
 import Home from "./route/Home.js"
-import Sample from "./route/Sample.js"
+import About from "./route/About"
+import Work from "./route/Work"
 import Header from "./layouts/Header"
 import Footer from "./layouts/Footer"
 
@@ -14,7 +16,9 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sample" element={<Sample />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Work" element={<Work />} />
+          <Route path="/Resume" element={<Home />} />
         </Routes>
         <Footer />
     </Router>
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import hed from "../JSON/header.json"
+import fot from "../JSON/footer.json"
+
 
 function Header() {
   return (
-    <div>Header</div>
+    // <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark ">
+      {/* <!-- Container wrapper --> */}
+      <div class="container-fluid mx-5">
+    
+        {/* <!-- Navbar brand --> */}
+        <a class="navbar-brand" href="#">Brand</a>
+  
+    
+        {/* <!-- Collapsible wrapper --> */}
+        <div class="">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    
+            {/* <!-- Link --> */}
+              {
+                hed.map( hed => {
+                  return(
+                    <li class="nav-item ">
+                    <a class="nav-link" href={hed.link}>{hed.title}</a>
+                  </li>
+                  )
+                })
+              }
+    
+          </ul>
+          </div>
+          <div>
+          {/* <!-- Icons --> */}
+          <ul class="navbar-nav d-flex flex-row me-1">
+            { fot && fot[0].icons.map( fot => {
+              return(
+                <li class="nav-item me-3 me-lg-0">
+                  <a class="nav-link" href="#"><i class={fot.icon}></i></a>
+                </li>
+              )
+            })
+          }
+          </ul>
+    
+        </div>
+      </div>
+      {/* <!-- Container wrapper --> */}
+    </nav>
+    // <!-- Navbar -->
+
   )
 }
 
