fadb3db3d1937e948c8280a423b29a8ee62d9ae1
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2022-07-04 15:46:57 +0200

    .

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -3,7 +3,6 @@ import Style from './BaseLayout.module.scss'
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
-import Portfolio from "./portfolio/Portfolio";
 import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
 
